477cc0bd9ddc7d014eb91a75ce4db5d234780810
poppatchara <patchara.pop@gmail.com>
2024-11-05 21:32:00 +0700

    backup

--- keyboards/pop_dactyl/keymaps/test/keymap.c
+++ keyboards/pop_dactyl/keymaps/test/keymap.c
@@ -22,8 +22,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_CAPS,    KC_A,       KC_S,       KC_D,       KC_F,       KC_G,
         KC_LSFT,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,
                                 KC_COMM,    KC_DOT,
+
                                             KC_1,       KC_2,       KC_3,
-                                            LT(Layer_utils, KC_4),       KC_5,
+                                            LT(Layer_utils,KC_4),   KC_5,
 
         // right hand
         KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_MINS,
